aef18e23a6e4f79ccb589e6e3557ca69a03908f9
moondevil999 <eng.999z@gmail.com>
2017-03-29 10:23:37 +0800

    refactor FoodCard

---
+++ src/components/FoodCard/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, Label, Image } from 'semantic-ui-react';
+import './index.css';
+
+const FoodCard = ({food}) => (
+	<Card fluid>
+		<Image src={food.image} />
+		<Label 
+			basic 
+			attached="top right" 
+			size="small"
+			className="price-label">
+			${food.price}
+		</Label>
+		<Card.Content>
+			<Card.Header>
+				{food.name}
+			</Card.Header>
+			<Card.Meta>
+				{food.total_reviews} reviews
+			</Card.Meta>
+		</Card.Content>
+	</Card>
+)
+
+export default FoodCard;
--- src/components/PlaceFood/index.js
+++ src/components/PlaceFood/index.js
@@ -1,50 +1,24 @@
 import React from 'react';
-import { Segment, Card, Grid, Label, Image } from 'semantic-ui-react';
-
-const FoodCard = ({food}) => (
-	<Card fluid>
-		<Image src={food.image} />
-		<Label 
-			basic 
-			attached="top right" 
-			size="tiny">
-			${food.price}
-		</Label>
-		<Card.Content>
-			<Card.Description>
-				<strong>
-					{food.name}
-				</strong>
-			</Card.Description>
-			<Card.Meta>
-				{food.total_reviews} reviews
-			</Card.Meta>
-		</Card.Content>
-	</Card>
-)
-
-// Continue
-function renderFoodCard(props) {
-	props.foods.map((food) => {
-		return (
-
-		)
-	})
-}
+import { Segment, Grid } from 'semantic-ui-react';
+import FoodCard from '../FoodCard';
 
 const PlaceFood = (props) => {
+
+	const renderFoodCard = props.foods.map((food) => {
+		return (
+			<Grid.Column stretched>
+				<FoodCard food={food} />
+			</Grid.Column>
+		)
+	});
+
 	return (
 		<Segment>
 			<h4>
 				Foods 
 			</h4>
 			<Grid columns={2}>
-				<Grid.Column stretched>
-					<FoodCard food={props.foods[0]} />
-				</Grid.Column>
-				<Grid.Column stretched>
-					<FoodCard food={props.foods[1]} />
-				</Grid.Column>
+				{renderFoodCard}
 			</Grid>
 		</Segment>
 	)
