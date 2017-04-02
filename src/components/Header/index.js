17903fd931bceee80c71c075d32ec713767fb93d
moondevil999 <eng.999z@gmail.com>
2017-04-02 23:47:03 +0800

    header nav responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
 import { Menu, Icon, Input, Button } from 'semantic-ui-react';
+import './index.css';
 
 class Header extends Component {
 	render () {
 		return (
-			<Menu borderless fixed="top">
-				<Menu.Item name="home-logo">
-					Burpple
-				</Menu.Item>
-				<Menu.Menu position="right">
-					<Menu.Item>
-						 <Input icon='search' placeholder='Search...' />
+			<div className="header-container">
+
+				<Menu borderless fixed="top" className="header-nav-large">
+					<Menu.Item name="home-logo">
+						Burpple
 					</Menu.Item>
-					<Menu.Item>
-						<Button color="green">Download App</Button>
+					<Menu.Menu position="right">
+						<Menu.Item>
+							 <Input icon='search' placeholder='Search...' />
+						</Menu.Item>
+						<Menu.Item>
+							<Button compact color="green">Download App</Button>
+						</Menu.Item>
+						<Menu.Item name="Sign up" />
+						<Menu.Item name="Log in" />
+					</Menu.Menu>
+				</Menu>
+
+				<Menu borderless fixed="top" className="header-nav-small">
+					<Menu.Item name="home-logo">
+						Burpple
 					</Menu.Item>
-					<Menu.Item name="Sign up" />
-					<Menu.Item name="Log in" />
-				</Menu.Menu>
-			</Menu>
+					<Menu.Menu position="right">
+						<Menu.Item>
+							<Button compact color="green">Download App</Button>
+						</Menu.Item>
+						<Menu.Item>
+							<Icon name="bars" />
+						</Menu.Item>
+					</Menu.Menu>
+				</Menu>
+
+			</div>
+
 		);
 	}
 }
