bab933867752d5b68aed4fe9e326fc875255e2dc
moondevil999 <eng.999z@gmail.com>
2017-04-02 17:17:45 +0800

    add more menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon, Input, Button } from 'semantic-ui-react';
 
 class Header extends Component {
 	render () {
 		return (
-			<Menu borderless fixed="top" size="large">
+			<Menu borderless fixed="top">
 				<Menu.Item name="home-logo">
 					Burpple
 				</Menu.Item>
-				<Menu.Item position="right">
-					<Icon name="bars" />
-				</Menu.Item>
+				<Menu.Menu position="right">
+					<Menu.Item>
+						 <Input icon='search' placeholder='Search...' />
+					</Menu.Item>
+					<Menu.Item>
+						<Button color="green">Download App</Button>
+					</Menu.Item>
+					<Menu.Item name="Sign up" />
+					<Menu.Item name="Log in" />
+				</Menu.Menu>
 			</Menu>
 		);
 	}
